68ec4bd684b25d31f6aeead4dc761aebb2c6bbec
Si Te Feng <fengsite@hotmail.com>
2017-07-31 17:31:08 -0400

    Added About and Terms of Service pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
+
+  get 'terms_of_service/index'
+
+  get 'privacy_policy/index'
+
   get 'landing/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
